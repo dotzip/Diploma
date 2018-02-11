73b8c8ffc2eb5b8df16795829d430c8006b91e00
dotzip <acapitalist123>
2018-02-11 20:28:32 +0300

    PCO login with timestamps

--- JMeter_Login/src/Login.groovy
+++ JMeter_Login/src/Login.groovy
@@ -1,21 +1,35 @@
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
+import org.apache.jmeter.samplers.SampleResult
 import java.util.concurrent.TimeUnit
 
 /**
  * @System.setProperty for browser
  * @WebDriver object for browser management
+ * @SampleResult for timestamps
  */
 
-System.setProperty("webdriver.chrome.driver", "path\\to\\browser-driver\\chromedriver.exe")
+System.setProperty("webdriver.chrome.driver", "path_to_browser_driver\\chromedriver.exe")
 
 WebDriver driver = new ChromeDriver()
 
+SampleResult result = new SampleResult()
+
 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS)
 
-driver.get("http://duckduckgo.com")
-driver.findElement(By.id("search_form_input_homepage")).click()
-driver.findElement(By.id("search_form_input_homepage")).clear()
-driver.findElement(By.id("search_form_input_homepage")).sendKeys("blazemeter")
-driver.findElement(By.id("search_button_homepage")).click()
+log.info("=== Login sampler ===")
+
+driver.get("localhost")
+driver.findElement(By.id("IDToken1")).click()
+driver.findElement(By.id("IDToken1")). sendKeys("john")
+driver.findElement(By.id("IDToken2")).click()
+driver.findElement(By.id("IDToken2")).sendKeys("Admin123!")
+
+result.sampleStart()
+driver.findElement(By.name("Login.Submit")).click()
+result.sampleEnd()
+
+log.info("Login's time: ${(result.endTime - result.startTime) / 1000} seconds")
+
+log.info("=== End ===")
